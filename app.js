4f317639491c06ed295c026b451353bcae78f1db
thapaRoyal <thaparoyal17@gmail.com>
2021-09-14 09:54:36 +0545

    javascript to display data

--- app.js
+++ app.js
@@ -1,18 +1,53 @@
-// UNSPLASH API
-const count = 10;
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
+// Unsplash API
+const count = 30;
+// Normally, don't store API Keys like this, but an exception made here because it is free, and the data is publicly available!
 const apiKey = 'U4mfoauS94G06NctgUbPr2C654pjPLSBG_glbwzAtVU';
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}
-`;
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
-// get photos from unsplash API
+// Helper Function to Set Attributes on DOM Elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
 
+// Create Elements For Links & Photos, Add to DOM
+function displayPhotos() {
+  // Run function for each object in photosArray
+  photosArray.forEach((photo) => {
+    // Create <a> to link to full photo
+    const item = document.createElement('a');
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
+    });
+    // Create <img> for photo
+    const img = document.createElement('img');
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+
+    // Put <img> inside <a>, then put both inside imageContainer Element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
+// Get photos from Unsplash API
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data);
+    photosArray = await response.json();
+    displayPhotos();
   } catch (error) {
-    // Catch error here
+    // Catch Error Here
   }
 }
 
